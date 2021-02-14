166694f829fdcfee3c518ebf749fd9e08d10adbc
SpyECho <bakchaloni@gmail.com>
2021-02-14 15:51:11 +0100

    add MDX markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 
 const Home = () => {
-  return <Layout>Hello world!</Layout>
+  return (
+    <Layout>
+      Hello world!
+      <div>
+        <Link to="/post">go to Post</Link>
+      </div>
+    </Layout>
+  )
 }
 
 export default Home
